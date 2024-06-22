e26eaa90d155b6bb3108fde3cd9d3df233a0df2b
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-06-22 13:08:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -208,14 +208,59 @@ if canvas_result.image_data is not None:
 model = tf.keras.models.load_model('mnist_cnn_model.h5')
 
 # Функция для предобработки изображения с холста
+# def preprocess_canvas_image(image_data):
+#     img = Image.fromarray((image_data * 255).astype('uint8'))
+#     img = img.convert('L')  # Преобразование в оттенки серого
+#     resized_image = img.resize((28, 28), Image.Resampling.BILINEAR)
+#     grayscaled_image = resized_image.convert("L")
+#     inverted_image = 255 - np.array(grayscaled_image)
+#     normalized_image = inverted_image / 255.0
+#     reshaped_image = normalized_image.reshape((1, 28, 28, 1))
+
+#     # Отображение предобработанных изображений с помощью Matplotlib
+#     fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(10, 6))
+
+#     # Скрытие меток осей для всех подграфиков
+#     for axis in ax.flat:
+#         axis.set_xticks([])
+#         axis.set_yticks([])
+
+#     # Исходное изображение
+#     ax[0, 0].set_title("Original Image")
+#     ax[0, 0].imshow(img, cmap='gray')
+
+#     # Измененное изображение (28 * 28)
+#     ax[0, 1].set_title("Resized Image")
+#     ax[0, 1].imshow(resized_image, cmap='gray')
+
+#     # Grayscale изображение
+#     ax[0, 2].set_title("Grayscale Image")
+#     ax[0, 2].imshow(grayscaled_image, cmap="gray")
+
+#     # Инвертированное изображение (текст белый, фон черный)
+#     ax[1, 0].set_title("Inverted Image")
+#     ax[1, 0].imshow(inverted_image, cmap="gray")
+
+#     # Нормализованное изображение (делим на 255, чтобы значения были от 0 до 1)
+#     ax[1, 1].set_title("Normalized Image")
+#     ax[1, 1].imshow(normalized_image, cmap="gray")
+
+#     # Измененная форма изображения
+#     ax[1, 2].set_title("Reshaped Image")
+#     ax[1, 2].imshow(reshaped_image.reshape((28, 28)), cmap="gray")
+
+#     st.pyplot(fig)
+
+#     return reshaped_image
+
 def preprocess_canvas_image(image_data):
-    img = Image.fromarray((image_data * 255).astype('uint8'))
+    img = Image.fromarray(image_data)
     img = img.convert('L')  # Преобразование в оттенки серого
-    resized_image = img.resize((28, 28), Image.Resampling.BILINEAR)
-    grayscaled_image = resized_image.convert("L")
-    inverted_image = 255 - np.array(grayscaled_image)
-    normalized_image = inverted_image / 255.0
-    reshaped_image = normalized_image.reshape((1, 28, 28, 1))
+    resized_image = img.resize((28, 28), Image.BILINEAR)  # Изменение размера
+    grayscaled_image = resized_image.convert("L")  # Преобразование в grayscale
+    inverted_image = 255 - np.array(grayscaled_image)  # Инверсия цветов
+    normalized_image = inverted_image / 255.0  # Нормализация значений
+    reshaped_image = normalized_image.reshape((1, 28, 28, 1))  # Изменение формы для модели CNN
 
     # Отображение предобработанных изображений с помощью Matplotlib
     fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(10, 6))
@@ -249,9 +294,11 @@ def preprocess_canvas_image(image_data):
     ax[1, 2].set_title("Reshaped Image")
     ax[1, 2].imshow(reshaped_image.reshape((28, 28)), cmap="gray")
 
-    st.pyplot(fig)
+    plt.tight_layout()
+    plt.show()
 
     return reshaped_image
+
 
 # Streamlit App
 st.title('MNIST Digit Classifier')
