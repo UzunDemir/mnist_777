e5c828e3cc3616c7c54b29688e1dd30cbb1368f5
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-06-24 08:25:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ st.markdown("""
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 100vh;
+        height: 10vh;
         text-align: center;
         flex-direction: column;
+        margin-top: 10vh;  /* отступ сверху */
     }
     </style>
     <div class="center">
