ae3c7fca5e22be6ebbebb1b00db36269eb5167b6
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-06-24 09:50:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown("""
     }
     </style>
     <div class="center">
-        st.markdown('<img src="https://github.com/UzunDemir/mnist_777/blob/main/200w.gif?raw=true">')
+        <img src="https://github.com/UzunDemir/mnist_777/blob/main/200w.gif?raw=true">
 
         st.divider()
         <h1>Классификатор рукописных цифр</h1>
